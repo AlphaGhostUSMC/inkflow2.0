24bce63eb4c623fb0c9671322391f7a3b462d030
AlphaGhostUSMC <Soulofninja69@gmail.com>
2023-06-04 17:47:39 -0700

    Login, Register Basic Design Complete

--- frontend/src/pages/loginPage.js
+++ frontend/src/pages/loginPage.js
@@ -3,6 +3,7 @@ import { Form } from "react-router-dom";
 export default function LoginPage() {
   return (
     <form className="login">
+      <h1>Login</h1>
       <input type="text" placeholder="Username" />
       <input type="password" placeholder="Password" />
       <button>Login</button>
